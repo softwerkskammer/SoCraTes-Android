574eec0d83be618bd0b3d3f74751dbad18297624
Johannes <johannes.w.seitz@gmail.com>
2014-08-06 23:01:05 +0200

    Add unit test target

---
+++ mobile/src/test/java/org/softwerkskammer/socrates/Test.java
@@ -0,0 +1,13 @@
+package org.softwerkskammer.socrates;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
+
+public class Test {
+
+    @org.junit.Test
+    public void failingTest() throws Exception {
+
+        assertThat("SoCraTes", is(equalTo("awesome")));
+    }
+}
